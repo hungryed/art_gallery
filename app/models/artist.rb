45569bd0bcdbd75d8e6dd892e2ead8bd903988b0
Joe Sutton <hungryed6@gmail.com>
2013-12-20 11:30:39 -0500

    Now have failing tests for artwork for Uninitialized constant Type and Collection

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,7 @@
 class Artist < ActiveRecord::Base
   belongs_to :style
-  has_many :artworks
+  has_many :artworks,
+    inverse_of: :artist
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :birth_place
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,11 +1,14 @@
 class Artwork < ActiveRecord::Base
-  belongs_to :artist
-  belongs_to :type
-  belongs_to :collection
+  belongs_to :artist,
+    inverse_of: :artworks
+  belongs_to :type,
+    inverse_of: :artworks
+  belongs_to :collection,
+    inverse_of: :artworks
   validates_presence_of :name
   validates_presence_of :artist
   validates_numericality_of :cost, greater_than_or_equal_to: 0, allow_nil: true
   validates_presence_of :type
-  validates_presence_of :collection
+
 
 end
